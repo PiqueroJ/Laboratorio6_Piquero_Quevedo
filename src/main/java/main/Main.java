f51e72f05ee4f06a6246f31593ab01482620cbc5
Juan Piquero <juan.piquero@alu.inspt.utn.edu.ar>
2026-06-06 15:23:09 -0300

    Creada la clase cliente

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package com.mycompany.laboratorio6_piquero_quevedo;
+package main;
 
 /**
  *
