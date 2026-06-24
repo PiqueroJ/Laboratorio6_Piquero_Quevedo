6b173b323bddba970ca74d00a7975c5e7c8e3eb6
Alumno <Alumno@172.16.1.171>
2026-06-24 12:41:26 -0300

    Empazado laboratorio 7 y creada fàbrica

---
+++ src/main/java/main/FabricaDePastas.java
@@ -0,0 +1,101 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class FabricaDePastas {
+
+    private String nombre = "Piquedo S.A.";
+    private ArrayList<Pedido> listaDePedidos;
+
+    public FabricaDePastas() {
+        this.listaDePedidos = new ArrayList();
+        
+    }
+
+    
+    
+    
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void menu() {
+        Scanner teclado = new Scanner(System.in);
+        int menu = -1;
+
+        do {
+            try {
+                System.out.println("===== " + nombre + " =====");
+                System.out.println("1. Agregar pedido");
+                System.out.println("2. Buscar pedido");
+                System.out.println("3. Listar pedidos");
+                System.out.println("4. Eliminar pedido");
+                System.out.println("5. Exportar pedidos a TXT");
+                System.out.println("6. Guardar pedidos (Serialización)");
+                System.out.println("7. Recuperar pedidos (Deserialización)");
+                System.out.println("8. Salir");
+                System.out.println("Opción: ");
+                menu = teclado.nextInt();
+
+                if (menu < 1 || menu > 8) {
+                    System.out.println("El numero ingresado es invalido");
+                }
+
+                switch (menu) {
+                    case 1 ->
+                        agregarPedido();
+                    case 2 ->
+                        buscarPedido();
+                    case 3->
+                        listarPedidos();
+                    case 4->
+                        eliminarPedido();
+                    case 5 ->
+                        exportarPedidos();
+                    case 6 ->
+                        guardarPedidos();
+                    case 7->
+                        recuperarPedidos();
+                        
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Tipo de dato incorrecto (Ingresó letras o formato numérico inválido).");
+            }
+            teclado.nextLine(); // Limpiamos el buffer del Scanner para evitar bucle infinito
+        } while (menu < 1 || menu > 3);
+    }
+
+    private void agregarPedido() {
+        listaDePedidos.add(new Pedido());
+    }
+
+    private void buscarPedido() {
+        
+    }
+
+    private void listarPedidos() {
+        for (Pedido ldp : listaDePedidos) {
+            ldp.mostrarPedido();
+            System.out.println("");
+        }
+    }
+
+    private void eliminarPedido() {
+        
+    }
+
+    private void exportarPedidos() {
+
+    }
+
+    private void guardarPedidos() {
+
+    }
+
+    private void recuperarPedidos() {
+
+    }
+
+}
